95fe5b8035136eb965d541455d6490d96a926410
devopslifter <tfinch@chef.io>
2018-11-05 12:21:07 +0000

    Added build steps and init hook

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -3,20 +3,18 @@ $pkg_origin="devopslifter"
 $pkg_version="0.1.0"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=@("Apache-2.0")
-$pkg_upsteam_url="https://github.com/nopSolutions/nopCommerce"
+$pkg_upsteam_url="https://github.com/devopslifter/nopCommerce"
 $pkg_description="NOP Commerce ASP.net app"
-
 
 $pkg_deps=@("core/dotnet-core")
 $pkg_build_deps=@("core/dotnet-core-sdk")
-
 
 $pkg_exports=@{
     "port"="port"
 }
 
-$pkg_binds=@{
-  "database"="username password port"
+# $pkg_binds=@{
+#   "database"="username password port"
 }
 
 function Invoke-SetupEnvironment {
@@ -24,10 +22,13 @@ function Invoke-SetupEnvironment {
 }
 function Invoke-Build{
   cp $PLAN_CONTEXT/../* $HAB_CACHE_SRC_PATH/$pkg_dirname -recurse -force
-  & "$(Get-HabPackagePath dotnet-core-sdk)\bin\dotnet.exe" restore
-  & "$(Get-HabPackagePath dotnet-core-sdk)\bin\dotnet.exe" build
+  & "$(Get-HabPackagePath dotnet-core-sdk)\bin\dotnet.exe" restore $HAB_CACHE_SRC_PATH/$pkg_dirname/src/NopCommerce.sln
+  & "$(Get-HabPackagePath dotnet-core-sdk)\bin\dotnet.exe" build $HAB_CACHE_SRC_PATH/$pkg_dirname/src/NopCommerce.sln
   if($LASTEXITCODE -ne 0) {
       Write-Error "dotnet build failed!"
-    dotnet restore "$HAB_CACHE_SRC_PATH\$pkg_dirname\nopCommerce-develop\src\NopCommerce.sln"
   }
+}
+
+function Invoke-Install {
+  & "$(Get-HabPackagePath dotnet-core-sdk)\bin\dotnet.exe" publish $HAB_CACHE_SRC_PATH/$pkg_dirname/src/NopCommerce.sln --output "$pkg_prefix/www"
 }
