8f5e129b3ec155a7ce955090401694ceb63ced8c
devopslifter <tfinch@chef.io>
2018-11-05 12:52:05 +0000

    removed db bind prior to db

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -11,15 +11,16 @@ $pkg_build_deps=@("core/dotnet-core-sdk")
 
 $pkg_exports=@{
     "port"="port"
-# }
+}
 
-# # $pkg_binds=@{
-# #   "database"="username password port"
+# $pkg_binds=@{
+#   "database"="username password port"
 # }
 
 function Invoke-SetupEnvironment {
   Set-RuntimeEnv "HAB_CONFIG_PATH" $pkg_svc_config_path
 }
+
 function Invoke-Build{
   cp $PLAN_CONTEXT/../* $HAB_CACHE_SRC_PATH/$pkg_dirname -recurse -force
   & "$(Get-HabPackagePath dotnet-core-sdk)\bin\dotnet.exe" restore $HAB_CACHE_SRC_PATH/$pkg_dirname/src/NopCommerce.sln
